32ff80b47da67d4bf3c2db5450ccb3ed1a588835
Mario Villaquiran <mariovillaquiran@marios-air.myfiosgateway.com>
2021-11-02 18:50:45 -0400

    worked on js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,30 @@
 var currentquestionindex = 0; 
 var time = questions.length * 15; 
 var timer ;
+
+var questionselement = document.getElementById("questions")
+var timerelement = document.getElementById ("time")
+var choiceselement = document.getElementById ("choices")
+var startbutton = document.getElementById("start")
+var submitbutton = document.getElementById("submit")
+
+function start (){
+var startscreenelement = document.getElementById("start-screen")
+startscreenelement.setAttribute("class","hide")
+questionselement.removeAttribute ("class")
+timer = setInterval(clock,1000)
+timerelement.textContent = time
+displayquestion()
+}
+function displayquestion () {
+    array.forEach(element => {
+        
+    });
+}
+function check () {
+
+}
+
+function end () {
+    
+}
