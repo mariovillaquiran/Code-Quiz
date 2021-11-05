0ca10a8327b05526d4ab48eb28ce48f06f901b2e
Mario Villaquiran <mariovillaquiran@marios-air.myfiosgateway.com>
2021-11-04 20:45:20 -0400

    high scores added

--- assets/questions.js
+++ assets/questions.js
@@ -1,7 +1,35 @@
 var questions = [
     {
-        title:"Question1", 
-        choices: ["Choice1","Choice2"],
-        answer: "Choice1"
+        title:"Arrays in JavaScript can be used to store _______.", 
+        choices: ["1. numbers and strings","2. other arrays","3.booleans","4. all of the above"],
+        answer: "4. all of the above"
+    }
+]
+var questions = [
+    {
+        title:"String values must be enclosed within ____ when being assigned to variables.", 
+        choices: ["1. commas","2. curly brakcets","3. quotes","4. parenthesis"],
+        answer: "2. curly brackets"
+    }
+]
+var questions = [
+    {
+        title:"A very useful tool used during development and debugging for printing content to the debugger is:", 
+        choices: ["1. JavaScript","2. terminal/bash","3. for loops","4. console.log"],
+        answer: "3. for loops"
+    }
+]
+var questions = [
+    {
+        title:"The condition in an if/else statement is enclosed with __________.", 
+        choices: ["1. quotes","2. curly brackets","3. parenthesis","4. square brackets"],
+        answer: "3. parenthesis"
+    }
+]
+var questions = [
+    {
+        title:"Commonly used data types DO NOT include:", 
+        choices: ["1. strings","2. booleans","3. alerts","4. numbers"],
+        answer: "2. booleans"
     }
 ]
